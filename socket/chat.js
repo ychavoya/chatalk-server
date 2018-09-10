687cedd1f50b05c4d770934d8a0768125eb44e9b
Yael Chavoya <realidadvirtualwm@gmail.com>
2018-09-10 17:12:30 -0500

    Obtención de ID y respuestas más completas

--- socket/chat.js
+++ socket/chat.js
@@ -3,17 +3,28 @@ const { io } = require('../server');
 io.on('connection', (client) => {
     console.log(client.id + ' conectado');
     
-    client.on('mensaje', (data, callback) =>{
-        client.broadcast.emit('mensaje', {
+    client.on('mensaje', (data, callback) => {
+        let mensaje = {
             usuario: client.id,
             mensaje: data.mensaje,
-            fecha: new Date()
-        });
+            fecha: new Date(),
+        }
+        client.broadcast.emit('mensaje', mensaje);
         console.log(`${client.id}: ${data.mensaje}`);
         callback({
             status: 200,
-            mensaje: 'Mensaje enviado',
-            fecha: new Date()
+            mensaje: 'Mensaje recibido',
+            fecha: new Date(),
+            data: mensaje,
+        });
+    });
+
+    client.on('getOwnId', (data, callback) => {
+        callback({
+            status: 200,
+            mensaje: 'ID enviada',
+            fecha: new Date(),
+            data: client.id,
         });
     });
 
